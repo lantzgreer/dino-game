9c4f518305abdb87c357fdf6bd7b6f89606ac051
lantzgreer <lantzgreer@gmail.com>
2022-01-01 02:49:29 -0600

    i give up
    this video sucks

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var character = document.getElementById("character");
-var block = document.getElementById("block")
+var block = document.getElementById("block");
 function jump() {
+    console.log("click");
     character.classlist.add("animate");
     setTimeout(function(){
         character.classList.remove("animate");
-    }, 500);
+    },500);
 }
